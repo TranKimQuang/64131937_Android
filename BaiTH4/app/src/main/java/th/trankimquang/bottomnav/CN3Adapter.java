c4fbc50b3604a26ea226d3ea3e9a496de719ca61
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2024-12-10 02:11:47 +0700

    CN 2 và CN 3

---
+++ BaiTH4/app/src/main/java/th/trankimquang/bottomnav/CN3Adapter.java
@@ -0,0 +1,45 @@
+package th.trankimquang.bottomnav;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class CN3Adapter extends RecyclerView.Adapter<CN3Adapter.ViewHolder> {
+
+    private List<String> mData;
+
+    public CN3Adapter(List<String> data) {
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cn3, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = mData.get(position);
+        holder.textView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.item_text);
+        }
+    }
+}
--- BaiTH4/app/src/main/java/th/trankimquang/bottomnav/CN3Fragment.java
+++ BaiTH4/app/src/main/java/th/trankimquang/bottomnav/CN3Fragment.java
@@ -4,14 +4,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.Arrays;
+import java.util.List;
 
 public class CN3Fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cn3, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_cn3, container, false);
+
+        // Setup RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.horizontal_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        // Data for RecyclerView
+        List<String> data = Arrays.asList("Item 1", "Item 2", "Item 3", "Item 4");
+
+        // Set adapter
+        CN3Adapter adapter = new CN3Adapter(data);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
