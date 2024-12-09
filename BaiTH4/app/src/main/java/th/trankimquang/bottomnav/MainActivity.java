d85e5675a18f3c7859196d7caaab7ab8cebd4b96
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2024-12-10 01:36:47 +0700

    layout

--- BaiTH4/app/src/main/java/th/trankimquang/bottomnav/MainActivity.java
+++ BaiTH4/app/src/main/java/th/trankimquang/bottomnav/MainActivity.java
@@ -1,24 +1,50 @@
 package th.trankimquang.bottomnav;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CN1Fragment()).commit();
+        }
+
+        // Handle item selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int id = item.getItemId();
+
+                if (id == R.id.navigation_cn1) {
+                    selectedFragment = new CN1Fragment();
+                } else if (id == R.id.navigation_cn2) {
+                    selectedFragment = new CN2Fragment();
+                } else if (id == R.id.navigation_cn3) {
+                    selectedFragment = new CN3Fragment();
+                } else if (id == R.id.navigation_cn4) {
+                    selectedFragment = new CN4Fragment();
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                }
+                return true;
+            }
         });
+
     }
 }
